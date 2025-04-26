46041ad13654f55cecd8b7e9fae5463284f56348
Rob Data <roberthernandezmarketing@gmail.com>
2025-04-26 12:44:51 -0400

    Adding html, other styles

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-const a = "Hello World";
+const a = "Hello beloved JS";
+const title = document.querySelector("h1").textContent;
+const subtitle = document.querySelector("h2").textContent;
 
 console.log(a);
+console.log("This is the title h1 [" , title, "] from DOM");
+console.log("This is the subtitle h2 [" , subtitle, "] from DOM");
