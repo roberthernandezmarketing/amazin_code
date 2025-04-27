20cc434bb26faec4f0e1714013b15fcb15c5ad80
Rob Data <roberthernandezmarketing@gmail.com>
2025-04-27 18:42:59 -0400

    Console b

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const a = "Hello beloved JS";
+const b = "Bye Bye beloved JS";
 const title = document.querySelector("h1").textContent;
 const subtitle = document.querySelector("h2").textContent;
 
 console.log(a);
 console.log("This is the title h1 [" , title, "] from DOM");
 console.log("This is the subtitle h2 [" , subtitle, "] from DOM");
+
+console.log(b);
